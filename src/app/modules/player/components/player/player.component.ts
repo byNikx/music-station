86e4f8d7e84a06e18661a4cad56f53758d63601f
Nikx Fabrizio <w3c.nadeem@gmail.com>
2019-01-11 02:21:59 +0530

    1

--- src/app/modules/player/components/player/player.component.ts
+++ src/app/modules/player/components/player/player.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'nx-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PlayerComponent implements OnInit {
 
